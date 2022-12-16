e9b189978b988f5aadd71644005c845aa9e4f0fa
Aikenkov <Fabio.rod.silva.spqr@gmail.com>
2022-12-16 16:51:46 -0300

    criando seeds de TicketTypes

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import dayjs from "dayjs";
 const prisma = new PrismaClient();
-
+import { TicketType } from "@prisma/client";
 async function main() {
   let event = await prisma.event.findFirst();
   if (!event) {
@@ -17,6 +17,36 @@ async function main() {
   }
 
   console.log({ event });
+
+  const types = await prisma.ticketType.findFirst({});
+
+  if (!types) {
+    await prisma.ticketType.createMany({
+      data: [
+        {
+          name: "online",
+          price: 100,
+          isRemote: true,
+          includesHotel: false,
+        },
+        {
+          name: "presencial",
+          price: 250,
+          isRemote: false,
+          includesHotel: false,
+        },
+        {
+          name: "com hotel",
+          price: 600,
+          isRemote: false,
+          includesHotel: true,
+        },
+      ],
+    });
+
+    const alltypes = await prisma.ticketType.findMany({});
+    console.log({ alltypes });
+  }
 }
 
 main()
