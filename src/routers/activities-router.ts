ae4d768911676163e51d6b8d895fa140cb5806bf
henriquesdias <henrique.dias1915@gmail.com>
2022-12-22 22:24:04 -0300

    feat: configure route '/activities' with method post

--- src/routers/activities-router.ts
+++ src/routers/activities-router.ts
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import { authenticateToken, validateBody } from "@/middlewares";
-import { getActivities, getActivitiesByDay, getDaysWithActivities } from "@/controllers";
-import { getActivitiesSchema } from "@/schemas";
+import { getActivities, getActivitiesByDay, getDaysWithActivities, postActivity } from "@/controllers";
+import { getActivitiesSchema, postActivitiesBookingSchema } from "@/schemas";
 
 const activitiesRouter = Router();
 //validateBody(getActivitiesSchema)
 
-activitiesRouter.all("/*", authenticateToken)
+activitiesRouter
+  .all("/*", authenticateToken)
   .get("/", validateBody(getActivitiesSchema), getActivities)
   .get("/days", getDaysWithActivities)
-  .get("/day/:day", getActivitiesByDay);
+  .get("/day/:day", getActivitiesByDay)
+  .post("/", validateBody(postActivitiesBookingSchema), postActivity);
 
 export { activitiesRouter };
--- src/schemas/activities-schemas.ts
+++ src/schemas/activities-schemas.ts
@@ -3,3 +3,7 @@ import Joi from "joi";
 export const getActivitiesSchema = Joi.object({
   day: Joi.date().required(),
 });
+
+export const postActivitiesBookingSchema = Joi.object({
+  activityId: Joi.number().required(),
+});
