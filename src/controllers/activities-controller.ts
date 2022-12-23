840cfbae809ffc337c9ebf60a3adadb97ec89779
Aikenkov <Fabio.rod.silva.spqr@gmail.com>
2022-12-22 23:50:56 -0300

    fix: remove unnusage route

--- src/controllers/activities-controller.ts
+++ src/controllers/activities-controller.ts
@@ -3,28 +3,6 @@ import { AuthenticatedRequest } from "@/middlewares";
 import activityService from "@/services/activities-service";
 import httpStatus from "http-status";
 
-export async function getActivities(req: AuthenticatedRequest, res: Response) {
-  const { userId } = req;
-  const { body } = req.params;
-  const day = new Date(body);
-
-  const isValidDate = Date.parse(body);
-
-  if (isNaN(isValidDate)) {
-    return res.sendStatus(httpStatus.BAD_REQUEST);
-  }
-
-  try {
-    const activities = await activityService.getActivities(Number(userId), day);
-    return res.status(httpStatus.OK).send(activities);
-  } catch (error) {
-    if (error.name === "NotFoundError") {
-      return res.sendStatus(httpStatus.NOT_FOUND);
-    }
-    return res.sendStatus(httpStatus.BAD_REQUEST);
-  }
-}
-
 export async function getDaysWithActivities(req: AuthenticatedRequest, res: Response) {
   const { userId } = req;
   try {
--- src/routers/activities-router.ts
+++ src/routers/activities-router.ts
@@ -1,13 +1,12 @@
 import { Router } from "express";
 import { authenticateToken, validateBody } from "@/middlewares";
-import { getActivities, getActivitiesByDay, getDaysWithActivities, postActivity } from "@/controllers";
+import { getActivitiesByDay, getDaysWithActivities, postActivity } from "@/controllers";
 import { getActivitiesSchema, postActivitiesBookingSchema } from "@/schemas";
 
 const activitiesRouter = Router();
 
 activitiesRouter
   .all("/*", authenticateToken)
-  .get("/:body", getActivities)
   .get("/days", getDaysWithActivities)
   .get("/day/:day", getActivitiesByDay)
   .post("/", validateBody(postActivitiesBookingSchema), postActivity);
