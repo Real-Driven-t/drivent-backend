5bc8a7d5eed042ddc553b8a617c99f2a31fe0e7e
Aikenkov <Fabio.rod.silva.spqr@gmail.com>
2023-01-04 15:36:11 -0300

    test: add oauth base testes

--- tests/integration/oauth.test.ts
+++ tests/integration/oauth.test.ts
@@ -1,11 +1,10 @@
 import app, { init } from "@/app";
 import { prisma } from "@/config";
-import { duplicatedEmailError } from "@/services/users-service";
 import { faker } from "@faker-js/faker";
 import dayjs from "dayjs";
 import httpStatus from "http-status";
 import supertest from "supertest";
-import { createEvent, createUser } from "../factories";
+import { createEvent } from "../factories";
 import { cleanDb } from "../helpers";
 
 beforeAll(async () => {
@@ -33,7 +32,6 @@ describe("POST /oauth/github/login", () => {
   describe("when body is valid", () => {
     const generateValidBody = () => ({
       email: faker.internet.email(),
-      password: faker.internet.password(6),
     });
 
     it("should respond with status 400 when there is no event", async () => {
@@ -59,50 +57,12 @@ describe("POST /oauth/github/login", () => {
         await createEvent();
       });
 
-      it("should respond with status 409 when there is an user with given email", async () => {
-        const body = generateValidBody();
-        await createUser(body);
-
-        const response = await server.post("/oauth/github/login").send(body);
-
-        expect(response.status).toBe(httpStatus.CONFLICT);
-        expect(response.body).toEqual(duplicatedEmailError());
-      });
-
-      it("should respond with status 201 and create user when given email is unique", async () => {
-        const body = generateValidBody();
-
-        const response = await server.post("/oauth/github/login").send(body);
-
-        expect(response.status).toBe(httpStatus.CREATED);
-        expect(response.body).toEqual({
-          id: expect.any(Number),
-          email: body.email,
-        });
-      });
-
       it("should not return user password on body", async () => {
         const body = generateValidBody();
 
         const response = await server.post("/oauth/github/login").send(body);
 
         expect(response.body).not.toHaveProperty("password");
-      });
-
-      it("should save user on db", async () => {
-        const body = generateValidBody();
-
-        const response = await server.post("/oauth/github/login").send(body);
-
-        const user = await prisma.user.findUnique({
-          where: { email: body.email },
-        });
-        expect(user).toEqual(
-          expect.objectContaining({
-            id: response.body.id,
-            email: body.email,
-          }),
-        );
       });
     });
   });
