9060daf89ee734e25f062adae0e81fad998a0fcd
henriquesdias <henrique.dias1915@gmail.com>
2023-01-12 13:15:55 -0300

    feat: adds a cache on event route

--- src/repositories/redis-database-repository/index.ts
+++ src/repositories/redis-database-repository/index.ts
@@ -1,4 +1,5 @@
 import { connectRedis } from "@/config";
+import { Event } from "@prisma/client";
 
 type days = {
   day: Date;
@@ -23,11 +24,24 @@ async function insertDaysActivities(newDate: Date, activities: any[]) {
   const redis = await connectRedis();
   return redis.setEx(newDate.toISOString(), 60 * 10, JSON.stringify(activities));
 }
+
+async function getEvent() {
+  const redis = await connectRedis();
+  return redis.get("event");
+}
+
+async function insertEvent(event: Event) {
+  const redis = await connectRedis();
+  return redis.setEx("event", 60 * 60 * 24, JSON.stringify(event));
+}
+
 const redisRepository = {
   getDays,
   insertDays,
   getDaysActivities,
   insertDaysActivities,
+  getEvent,
+  insertEvent,
 };
 
 export default redisRepository;
